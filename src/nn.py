451fb533aba081bd6a125ad03a02f51f9111e66e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2024-11-19 17:01:27 -0500

    Implement a Multi-Layer Perceptron using Neurons and Layers

---
+++ src/nn.py
@@ -0,0 +1,43 @@
+import random
+from src.value import Value
+
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+        
+    def __call__(self, x):
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        
+        return out
+    
+    def parameters(self):
+        return self.w + [self.b]
+    
+    
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+        
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        
+        return out[0] if len(out) == 1 else out
+    
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
